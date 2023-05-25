b9f871a5d7e79ef3882895e602eaeb0abdae0ac8
Lensegrav <Gilbert.Lensegrav@dfw.wa.gov>
2023-05-25 13:14:27 -0700

    minor updates

--- spi_sendemails_dev.R
+++ spi_sendemails_dev.R
@@ -4,9 +4,12 @@
 rm(list=ls(all=TRUE))
 
 library(Microsoft365R)
+library(blastula)
 
 my_outlook <- get_business_outlook()
 my_email <- my_outlook$create_email(content_type = "html")$
   set_body("<p>This is my email body <strong>with bold text</strong>.</p>")$
   set_subject("My 2nd email subject")$
-  set_recipients(to = c("recipient1@mail.com", "recipient2@mail.com"))
+  set_recipients(to = c("gilbert.lensegrav@dfw.wa.gov"))
+
+my_email$send()
